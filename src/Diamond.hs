52bfb892c4837cf46abfb433397dc27a3e66a766
Udit Karode <udit.karode@gmail.com>
2022-02-02 20:39:05 +0530

    cleanup(Diamond): simplify

--- src/Diamond.hs
+++ src/Diamond.hs
@@ -4,13 +4,15 @@ import qualified Commands.Create as CreateCommand (create)
 import qualified Commands.Manual as ManualCommand (manual)
 import qualified Commands.Mount as MountCommand (mount)
 import SystemUtils (CliArgs (CliArgs), bail)
-import Transaction (Transaction (runTransaction))
+import Transaction (Command, Transaction (runTransaction))
+
+runCommand :: Command -> IO ()
+runCommand cmd = runTransaction cmd [] >> pure ()
 
 diamond :: CliArgs -> IO ()
 diamond (CliArgs create mount manual remove) =
   if
-      | create -> runTransaction CreateCommand.create [] >> pure ()
-      | mount -> runTransaction MountCommand.mount [] >> pure ()
-      | manual -> runTransaction ManualCommand.manual [] >> pure ()
-      | create && isJust remove -> bail "Cannot remove and create at the same time!"
+      | create -> runCommand CreateCommand.create
+      | mount -> runCommand MountCommand.mount
+      | manual -> runCommand ManualCommand.manual
       | otherwise -> bail "Invalid or no command! Please read --help"
