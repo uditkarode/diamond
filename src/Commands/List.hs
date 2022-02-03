110fce2fc3d31f9c5e425c6d489c94f9c7c88ca1
Udit Karode <udit.karode@gmail.com>
2022-02-03 21:17:49 +0530

    feat(Commands/List): also show mount status

--- src/Commands/List.hs
+++ src/Commands/List.hs
@@ -5,7 +5,7 @@ import Logger (logErrorMln, logInfoLn, logSuccessMln)
 import System.Console.Pretty (Color (Green, Red), Pretty (color, style), Style (Bold))
 import SystemUtils (Data (entries), DataEntry (diskImagePrefix, name, prefix), readData)
 import Transaction (Command, Transaction (Transaction))
-import Utils (foldl, isServiceActive)
+import Utils (contains, foldl, isServiceActive, run)
 
 formatForLog :: [(Text, Text)] -> Text
 formatForLog arr = T.dropEnd 1 (foldl arr "" $ \curr acc -> acc <> style Bold (fst curr) <> ": " <> snd curr <> "\n")
@@ -16,14 +16,16 @@ bcol c = style Bold . color c
 list :: Command
 list = do
   d <- entries <$> readData
+  mntd <- run "mount" []
   forM_ d $ \v -> do
     running <- isServiceActive $ name v
-    let r = if running then bcol Green "yes" else bcol Red "no"
+    let yn bool = if bool then bcol Green "yes" else bcol Red "no"
     let fn = liftIO . if running then logSuccessMln "" else logErrorMln ""
     fn . formatForLog $
       [ ("name", name v),
         ("mountpoint", prefix v <> "/mountpoint"),
         ("disk image", diskImagePrefix v <> "/" <> name v <> ".img"),
-        ("running", r)
+        ("mounted", yn $ mntd `contains` (name v <> ".img")),
+        ("running", yn running)
       ]
     putTextLn ""
